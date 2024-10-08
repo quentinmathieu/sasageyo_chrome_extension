710c884a51de717dbb8bdd5a37165d6dcd5ee232
quentinmathieu <quentin.mth@gmail.com>
2024-10-08 07:36:53 +0200

    init

---
+++ content.js
@@ -0,0 +1,9 @@
+document.documentElement.classList.add('sasageyo');
+
+let link = document.createElement('link');
+link.type = 'text/css';
+link.rel = 'stylesheet';
+
+document.head.appendChild(link);
+
+link.href = 'https://attack-on-titan.quentin-mathieu.fr/sasageyo.css';

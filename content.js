30df617ddcfa95404546da9f70e27a136a1025d3
quentinmathieu <quentin.mth@gmail.com>
2024-10-10 16:27:01 +0200

    trigger animation on extension click

--- content.js
+++ content.js
@@ -1,23 +1,20 @@
 chrome.runtime.onMessage.addListener(
     (request, sender, sendResponse) => {
-      if (request.message === "hi"){
-          sendResponse({message: "hi to you"});
-          return true;
-      }
-      else{
-        sendResponse({message: "meh"});
-        return false;
-      }
+     
+    
 
-    });
 
-// document.documentElement.classList.add('sasageyo');
+document.documentElement.classList.add('sasageyo');
 
-// let link = document.createElement('link');
-// link.type = 'text/css';
-// link.rel = 'stylesheet';
+let link = document.createElement('link');
+link.type = 'text/css';
+link.rel = 'stylesheet';
 
-// document.head.appendChild(link);
+document.head.appendChild(link);
 
-// link.href = 'https://attack-on-titan.quentin-mathieu.fr/sasageyo.css';
+link.href = 'https://attack-on-titan.quentin-mathieu.fr/sasageyo.css';
+
+
+
+});
 
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,11 +1,15 @@
-document.body.style.backgroundColor="red";
-
-
-
-(async () => {
-    const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
-    const response = await chrome.tabs.sendMessage(tab.id, {message: "ho"});
-    // do something with response here, not outside the function
-    console.log(response);  
-    })();
+// const checkbox = document.getElementById('check-box')
+// console.log(checkbox)
+// checkbox.addEventListener('change', (event) => {
+//   if (event.currentTarget.checked) {
+    (async () => {
+        const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+        const response = await chrome.tabs.sendMessage(tab.id, {html: "youhou"});
+        // do something with response here, not outside the function
+        console.log(response.html);  
+      })();
+//   } else {
+//     alert('not checked');
+//   }
+// })
 
